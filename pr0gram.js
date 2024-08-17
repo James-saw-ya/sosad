d73da0fbddc36c9f4ef89f10a726703c18820d63
James-saw-ya <dens_pickles.0v@icloud.com>
2024-08-17 08:32:27 +0200

    Update pr0gram.js

--- pr0gram.js
+++ pr0gram.js
@@ -3,7 +3,14 @@ const fs = require('node:fs');
 try {
   const pd = fs.readFileSync('pd.json', 'utf8');
   const sd = fs.readFileSync('sd.json', 'utf8');
-  console.log(sd.substring(0,8));
+  
+  fs.writeFile('data.json', "HELLO", err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("written successfully")
+  }
+});
 } catch (err) {
   console.error(err);
 }
