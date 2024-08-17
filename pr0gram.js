1bb3bf5215bbd14fe20839138c3154d9a3fbc45b
James-saw-ya <dens_pickles.0v@icloud.com>
2024-08-17 19:18:17 +0200

    Update pr0gram.js

--- pr0gram.js
+++ pr0gram.js
@@ -24,7 +24,8 @@ function filterData(sd,pd){
             !player.isSleepingTent) || player.last_duel) &&
             player.holiday_duel &&
             (!(new Date(player.nextBattleAtLocation*1000) > new Date()) || player.last_duel) &&
-            a.includes(player.alliance_id) 
+            a.includes(player.alliance_id) &&
+            getPosition(player) != null
        )
     
     relevantData.forEach(p => p["hp"] = pData.health.filter(entry => entry.name == p.name)[0].skill_level
